5aba46ac0fa55e4d354a6fabff025f0a7705ed36
ShubhamVadher <vadhershubhambhai@gmail.com>
2025-07-08 23:12:01 +0400

    commit

--- server/middleware/isloggedin.js
+++ server/middleware/isloggedin.js
@@ -5,7 +5,7 @@ const key=process.env.JWT
 const isloggedin=async(req,res,next)=>{
     try{
         if(!req.cookies.token||req.cookies.token==''){
-        window.alert('plzzz login first');
+        //window.alert('plzzz login first');
         return res.status(500).json({message:"You must login first"});
     }
     const data=jwt.verify(req.cookies.token,key);
